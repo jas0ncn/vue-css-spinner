9475f0edc6bc57170ffda4b718cf62bffe56ca13
Jas0ncn <ceocjy@vip.qq.com>
2016-02-20 01:14:13 +0800

    update demo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-	<spinner></spinner>
+	<h1>circleLine</h1>
+	<div class="spinner1">
+		<spinner1></spinner1>
+	</div>
+	<h2>dot</h2>
+	<div class="spinner2">
+		<spinner2></spinner2>
+	</div>
 </template>
 
 <script>
-import spinner from './components/circleLine'
+import spinner1 from './components/circleLine'
+import spinner2 from './components/dot'
 
 export default {
     components: {
-        spinner
+        spinner1,
+        spinner2
     }
 }
 </script>
@@ -20,5 +29,15 @@ export default {
 body {
     font-family: Helvetica, sans-serif;
     overflow-x: hidden;
+}
+h2 {
+	position: absolute;
+	top: 80px;
+	display: block;
+}
+.spinner2 {
+	position: absolute;
+	top: 120px;
+	width: 100%;
 }
 </style>
